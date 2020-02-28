8114002550a352fb8e0dd96c74a938bd4144d46f
Naimul Hasan <naimulhasannaim0194@gmail.com>
2020-02-29 05:03:31 +0600

    Update Segment_Tree with Lazy (sum).cpp

--- Graph and tree/Segment_Tree with Lazy (sum).cpp
+++ Graph and tree/Segment_Tree with Lazy (sum).cpp
@@ -1,36 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ull            unsigned long long
-#define ll             long long
-#define vi             vector<int>
-#define umap           unordered_map
-#define pb             push_back
-#define mpp             make_pair
-#define pii            pair<int,int>
-#define vit            vector<int> :: iterator
-#define sit            set<int> :: iterator
-#define ff             first
-#define ss             second
-#define endl           '\n'
-#define dbg(x)     cerr << #x << " is " << x << endl
-#define dbgg(x,y)     cerr << #x << " is " << x <<"    "<<#y<<" is "<<y<< endl
+
 const int N=2e5+5;
 int tree[N*4],lazy[N*4],arr[N];
-void build_tree(int node, int start, int end)
-{
-    if(start==end){
-        tree[start]=arr[start];
-        return;
-    }
-    int mid=(start+end)/2;
-    build_tree(node*2,start,mid);
-    build_tree(node*2+1 ,mid+1, end);
-    tree[node]=tree[node*2]+tree[node*2+1];
-}
+
 void range_update(int node, int start, int end, int l, int r, int x)
 {
     if(lazy[node]){
-        tree[node]+=(start-end+1)*lazy[node];
+        tree[node]+=(end-start+1)*lazy[node];
         if(start!=end){
             lazy[node*2]+=lazy[node];
             lazy[node*2+1]+=lazy[node];
@@ -39,7 +16,7 @@ void range_update(int node, int start, int end, int l, int r, int x)
     }
     if(end<l or r<start)return;
     if(l<=start and end<=r ){
-        tree[node]+=(start-end+1)*x;
+        tree[node]+=(end-start+1)*x;
         if(start!=end){
             lazy[node*2]+= x;
             lazy[node*2+1]+= x;
@@ -52,6 +29,36 @@ void range_update(int node, int start, int end, int l, int r, int x)
     tree[node] = tree[node*2]+tree[node*2+1];
 
 }
+long long query(int node, int start, int end, int l, int r)
+{
+    if(end<l or r<start)return 0;
+    if(lazy[node]){
+        tree[node]+=(end-start+1)*lazy[node];
+        if(start!=end){
+            lazy[node*2]+=lazy[node];
+            lazy[node*2+1]+=lazy[node];
+        }
+        lazy[node]=0;
+    }
+    if(l<=start and end<=r)return tree[node];
+    int mid=(start+end)/2;
+    long long q1 = query(node*2,start,mid,l,r);
+    long long q2 = query(node*2+1,mid+1,end,l,r);
+    return (q1+q2);
+}
+
+void build_tree(int node, int start, int e)
+{
+    if(start==e){
+        tree[node] = arr[start];
+        return;
+    }
+    int mid=(start+e)/2;
+    build_tree(node*2,start,mid);
+    build_tree(node*2+1 ,mid+1, e);
+    tree[node] = tree[node*2]+tree[node*2+1];
+}
+
 int main()
 {
     int n;
@@ -65,6 +72,10 @@ int main()
         if(op==1){
             cin>>l>>r>>x;
             range_update(1,1,n,l,r,x);
+        }
+        else {
+            cin>>l>>r;
+            cout<<query(1,1,n,l,r)<<endl;
         }
     }
 
